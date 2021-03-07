c8b24d92a2202dafd893a135fd620973e78d3786
Logan Sage <lvdsage@gmail.com>
2021-03-06 19:37:37 -0700

    service do be wokring doe

---
+++ public/service-worker.js
@@ -0,0 +1,88 @@
+const FILES_TO_CACHE = [
+    "/icons/icon-192x192.png", 
+    "/icons/icon-512x512.png",
+    "/", 
+    "/index.html", 
+    "/index.js",
+     
+    "/styles.css"
+  ];
+    
+  const STATIC_CACHE = "static-cache-v1";
+  const RUNTIME_CACHE = "runtime-cache";
+  const DATA_CACHE_NAME = "data-cache-v1";
+  
+
+
+
+  self.addEventListener("install", event => {
+    event.waitUntil(
+      caches
+        .open(STATIC_CACHE)
+        .then(cache => cache.addAll(FILES_TO_CACHE))
+        .then(() => self.skipWaiting())
+    );
+  });
+    
+
+
+  self.addEventListener("activate", event => {
+    const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
+    event.waitUntil(
+      caches
+        .keys()
+        .then(cacheNames => {
+          return cacheNames.filter(
+            cacheName => !currentCaches.includes(cacheName)
+          );
+        })
+        .then(cachesToDelete => {
+          return Promise.all(
+            cachesToDelete.map(cacheToDelete => {
+              return caches.delete(cacheToDelete);
+            })
+          );
+        })
+        .then(() => self.clients.claim())
+    );
+  });
+    
+  self.addEventListener("fetch", event => {
+    if (
+      event.request.method !== "GET" ||
+      !event.request.url.startsWith(self.location.origin)
+    ) {
+      event.respondWith(fetch(event.request));
+      return;
+    }
+
+
+
+    if (event.request.url.includes("/api/transaction")) {
+    event.respondWith(
+      caches.open(RUNTIME_CACHE).then(cache => {
+        return fetch(event.request)
+          .then(response => {
+            cache.put(event.request, response.clone());
+            return response;
+          })
+          .catch(() => caches.match(event.request));
+      })
+    );
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+        return caches.open(RUNTIME_CACHE).then(cache => {
+          return fetch(event.request).then(response => {
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  });
